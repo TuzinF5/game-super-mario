ee5028215f711c1ccfda0cf541691dc46861e0e4
Arthur Teixeira <tuzinf5@gmail.com>
2022-06-03 19:20:29 -0300

    Criando componente Main

---
+++ src/pages/Main.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '../styles/main.css';
+
+export default class Main extends Component {
+  render() {
+    return (
+      <main>
+        <section></section>
+      </main>
+    );
+  }
+}
