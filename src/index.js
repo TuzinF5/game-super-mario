66e306bc4718da32c218722138324c29af5d892a
Arthur Teixeira <tuzinf5@gmail.com>
2022-06-03 17:50:34 -0300

    Importando componente Main

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import Main from './pages/Main';
+import './styles/index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Main />
   </React.StrictMode>
 );
