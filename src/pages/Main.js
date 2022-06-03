c5e0ab5d0d10194c8034bf107d1b3e45b2759ff7
Arthur Teixeira <tuzinf5@gmail.com>
2022-06-03 19:22:35 -0300

    Adicionando imagens a página via estado

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
+import mario from '../images/mario.gif';
+import pipe from '../images/pipe.png';
 import '../styles/main.css';
 
 export default class Main extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      srcMario: mario,
+      srcPipe: pipe,
+    };
+  }
+
   render() {
+    const { srcMario, srcPipe } = this.state;
+
     return (
-      <main>
-        <section></section>
+      <main className="main-game">
+        <section className="game-board">
+          <img className="img-mario" src={srcMario} alt="super mario" />
+          <img className="img-pipe" src={srcPipe} alt="tubo super mario" />
+        </section>
       </main>
     );
   }
